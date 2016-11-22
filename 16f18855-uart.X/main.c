fe6ad52b76083d2c9f2f548c15f3d42aafb17b46
mittens <brentperkins63@hotmail.com>
2016-11-21 22:48:46 -0700

    just toying with the uart bridge. displays AB then GF each second

---
+++ 16f18855-uart.X/main.c
@@ -0,0 +1,32 @@
+#include "mcc_generated_files/mcc.h"
+#define carriageReturn 0x0D
+#define nullChar    0x00
+bool toggle = true;
+void main(void)
+{
+    SYSTEM_Initialize();
+    //INTERRUPT_GlobalInterruptEnable();
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    while (1)
+    {
+        if (TMR6_HasOverflowOccured())
+        {
+            if (toggle)
+            {
+                EUSART_Write(0x41); //A
+                EUSART_Write(0x42); //B
+                EUSART_Write(carriageReturn);
+                toggle = false;
+            }
+            else
+            {
+                EUSART_Write(0x46); //F
+                EUSART_Write(0x47); //G
+                EUSART_Write(carriageReturn);
+                toggle = true;
+            }
+
+        }
+    }
+}
