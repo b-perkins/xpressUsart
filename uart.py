b2db6628aac335529f0b14195b8cab3dc8152755
devsys ubuntuLaptop <devsys@ubuntLaptop>
2016-11-22 14:58:41 -0700

    added the latest of the python script as well as project configs xml

--- uart.py
+++ uart.py
@@ -1,16 +1,21 @@
 import serial
 
-byte = b'G'	# character 'G'
-ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=0, rtscts=0)
+byte = b"G"	# character 'G'
+buildString = ""
+ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None)
 print(ser.name)         # check which port was really used
+#byte.encode('ascii')
 
-print("sending 'G' char as: ", byte)
-ser.write(b"byte")     # write a string
+while True:
+	print("sending 'G' char as: ", byte)
+	ser.write(byte)     # write a string
 
-byte.encode('ascii')
-print("sending 'G' char as: ", byte)
-ser.write(b"byte")     # write a string
-print("reading response")
-s = ser.read(100)
-print("returned val: ",s)
+	#byte.encode('ascii')
+	#print("sending 'G' char as: ", byte)
+	#ser.write(b"byte")     # write a string
+	print("reading response")
+	s = ser.read(2)
+	print("returned val: ",s)
+
+print("quit")
 ser.close()             # close port
