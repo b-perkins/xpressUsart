732a441cdd633a4b232f872634f8b7c513792b4c
b-perkins <brentperkins63@hotmail.com>
2016-11-22 21:36:11 -0700

    trying a loop to build a returned string

--- uart.py
+++ uart.py
@@ -1,7 +1,8 @@
 import serial
 
 byte = b"G"	# character 'G'
-buildString = ""
+buildString = []
+
 ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None)
 print(ser.name)         # check which port was really used
 #byte.encode('ascii')
@@ -14,8 +15,11 @@ while True:
 	#print("sending 'G' char as: ", byte)
 	#ser.write(b"byte")     # write a string
 	print("reading response")
-	s = ser.read(2)
-	print("returned val: ",s)
+	for val in range(0,5,1):
+		s = ser.read(1)
+		buildString.append(s)
+	temperature = ''.join(l)
+	print("returned val: ",temperature)
 
 print("quit")
 ser.close()             # close port
