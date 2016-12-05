5a62988ffe12eff946007dd2c59004dc87ae23b0
esefa <esefa@home>
2016-12-05 06:26:54 +0000

    shoehorned a bit further. now it converts the two integer values read from xpress into a decimal string. then from that into a fahrenheit value, which is printed and written to the user_annotate.txt file in the pi directory /dev/shm/mjpeg/user_annotate.txt. It works! Looks like as long as this script is running that portion of the camera display will not show up, makes sense since the file is open. once this is done it shows up right away. Next steps will be to make the script run silently and on a cron job. maybe simplify the FW a bit too. and then finally a mount, and we're done!

--- uart.py
+++ uart.py
@@ -1,15 +1,18 @@
 import serial
 import subprocess
 
-celciusH, celciusL = 0, 0
+celsiusH, celsiusL = 0, 0
+celsius = ""
+fahrenheit = 0.0
+go = True
 
-ser = serial.Serial('COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)	#	If on win
-# ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None)	#	linux
+# ser = serial.Serial('COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)	#	If on win
+ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None)	#	linux
 print(ser.name)  # check which port was really used
-resulttxt = open("temp.txt", "w")	#the output file
+resulttxt = open("/dev/shm/mjpeg/user_annotate.txt", "w")	#the output file
 
-while True:
-	celciusH, celciusL = 0, 0
+while go:
+	celsiusH, celsiusL = 0, 0
 	print("Beginning - ping device by sending 'G' char. Waiting...")
 	goBIT = input()
 	if goBIT == 'G':
@@ -17,20 +20,24 @@ while True:
 
 	startBit = ser.read(1)  # Read 1 byte
 	if startBit.decode("utf-8") == 'K':  # K means the next to chars will be temp hi/lo
-		celciusH = ser.read(1)
-		celciusL = ser.read(1)
-		celciusH = ord(celciusH)
-		celciusL = ord(celciusL)
+		celsiusH = ser.read(1)
+		celsiusL = ser.read(1)
+		celsiusH = ord(celsiusH)	#converts to integer
+		celsiusL = ord(celsiusL)
 
-		print("temp is: ", celciusH, ".", celciusL)
-		resulttxt.write(str(celciusH))
-		resulttxt.write(".")
-		resulttxt.write(str(celciusL))
-		resulttxt.close()
+		celsius += str(celsiusH)	#create a decimal string
+		celsius += '.'
+		celsius += str(celsiusL)
+		print("tempString in C = ",celsius)
+		fahrenheit = (float(celsius)*9) / 5.0 + 32	#convert to fahrenheit (needs float)
+		print("temp in F = ", fahrenheit)
+		resulttxt.write(str(fahrenheit))
+		resulttxt.close()			#note - txt doesnt show up unless file is closed
 
 		lookingForL = ser.read(1)
-		if lookingForL.decode("utf-8") == 'L':
+		if lookingForL.decode("utf-8") == 'L':	#arbitrary stop character
 			print("DONE")
+			go = False
 		else:
 			print("something's not right")
 	else:
