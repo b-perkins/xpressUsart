19c49ecf05bf6754e87e0323dd1306ac6b3f1eb9
Brent Perkins <Brent Perkins>
2016-12-05 20:39:39 -0700

    much more stripped down version which runs through once and quits! See previous version for development history, since I didnt bother making a branch for this change. I'm now assuming that this script is called by cron at an interval, so here we just send the start bit 'G' and then read out from the xpress, then convert to fahrenheit and dump the resulting float into the user_annotate.txt for RPi_cam to pick up. the cron job is every minute.

--- uart.py
+++ uart.py
@@ -4,44 +4,27 @@ import subprocess
 celsiusH, celsiusL = 0, 0
 celsius = ""
 fahrenheit = 0.0
-go = True
 
 # ser = serial.Serial('COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)	#	If on win
 ser = serial.Serial('/dev/ttyACM0',baudrate=9600,bytesize=8, parity='N', stopbits=1, timeout=None)	#	linux
-print(ser.name)  # check which port was really used
 resulttxt = open("/dev/shm/mjpeg/user_annotate.txt", "w")	#the output file
 
-while go:
-	celsiusH, celsiusL = 0, 0
-	print("Beginning - ping device by sending 'G' char. Waiting...")
-	goBIT = input()
-	if goBIT == 'G':
-		ser.write(b'G')  # write charg G as a byte
+ser.write(b'G')  # write charg G as a byte
 
-	startBit = ser.read(1)  # Read 1 byte
-	if startBit.decode("utf-8") == 'K':  # K means the next to chars will be temp hi/lo
-		celsiusH = ser.read(1)
-		celsiusL = ser.read(1)
-		celsiusH = ord(celsiusH)	#converts to integer
-		celsiusL = ord(celsiusL)
+startBit = ser.read(1)  # Read 1 byte
+if startBit.decode("utf-8") == 'K':  # K means the next to chars will be temp hi/lo
+	celsiusH = ser.read(1)
+	celsiusL = ser.read(1)
+	celsiusH = ord(celsiusH)	#converts to integer
+	celsiusL = ord(celsiusL)
 
-		celsius += str(celsiusH)	#create a decimal string
-		celsius += '.'
-		celsius += str(celsiusL)
-		print("tempString in C = ",celsius)
-		fahrenheit = (float(celsius)*9) / 5.0 + 32	#convert to fahrenheit (needs float)
-		print("temp in F = ", fahrenheit)
-		resulttxt.write(str(fahrenheit))
-		resulttxt.close()			#note - txt doesnt show up unless file is closed
+	celsius += str(celsiusH)	#create a decimal string
+	celsius += '.'
+	celsius += str(celsiusL)
 
-		lookingForL = ser.read(1)
-		if lookingForL.decode("utf-8") == 'L':	#arbitrary stop character
-			print("DONE")
-			go = False
-		else:
-			print("something's not right")
-	else:
-		print("expected begginging char K but didnt get it")
+	fahrenheit = (float(celsius)*9) / 5.0 + 32	#convert to fahrenheit (needs float)
 
-print("quit")
+	resulttxt.write(str(fahrenheit))
+	resulttxt.close()			#note - txt doesnt show up unless file is closed
+
 ser.close()  # close port
